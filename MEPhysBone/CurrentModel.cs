8579d26ce951a69bb34cab07d4e3bdd01e24eeff
PAMEH-2045 <1239g.g.g1239@gmail.com>
2026-04-06 15:28:28 +0300

    Add PhysBoneGrabHelper form ASDK

---
+++ MEPhysBone/CurrentModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using UnityEngine;
+
+public class CurrentModel
+{
+    public static GameObject ModelGO { get; private set; }
+    public static Transform ModelRoot { get; private set; }
+
+    static readonly float avatarScanInterval = 0.25f;
+    static float nextAvatarScan;
+
+    public static void OnAwake()
+    {
+        var modelRootGO = GameObject.Find("Model");
+        if (modelRootGO != null)
+            ModelRoot = modelRootGO.transform;
+    }
+    public static void OnUpdate()
+    {
+        if (Time.unscaledTime >= nextAvatarScan)
+        {
+            UpdateCurrentAvatar();
+            nextAvatarScan = Time.unscaledTime + avatarScanInterval;
+        }
+    }
+    static void UpdateCurrentAvatar()
+    {
+        if (!ModelRoot) return;
+
+        for (int i = 0; i < ModelRoot.childCount; i++)
+        {
+            var child = ModelRoot.GetChild(i).gameObject;
+            if (!child.activeInHierarchy) continue;
+            if (ModelGO == child) return;
+            ModelGO = child;
+
+            UpdateAvatarComponents();
+
+            return;
+        }
+    }
+    static void UpdateAvatarComponents()
+    {
+        AvatarBigScreenHandlerProxy.Inst = GetComponent<AvatarBigScreenHandler>();
+    }
+    public static T GetComponent<T>() where T : Component
+    => ModelGO.GetComponent<T>();
+
+
+    public static class AvatarBigScreenHandlerProxy
+    {
+        public static AvatarBigScreenHandler Inst;
+
+        static readonly Func<AvatarBigScreenHandler, bool> _isBigScreenActive = MakeGetter<AvatarBigScreenHandler, bool>("isBigScreenActive");
+        public static bool isBigScreenActive
+        {
+            get => _isBigScreenActive(Inst);
+        }
+    }
+    static Func<TInstance, TField> MakeGetter<TInstance, TField>(string fieldName)
+    {
+        var fieldInfo = typeof(TInstance).GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+        var instanceParam = Expression.Parameter(typeof(TInstance));
+        MemberExpression fieldAccess;
+        if (fieldInfo.IsStatic)
+            fieldAccess = Expression.Field(null, fieldInfo);
+        else
+            fieldAccess = Expression.Field(instanceParam, fieldInfo);
+
+        var lambda = Expression.Lambda<Func<TInstance, TField>>(fieldAccess, instanceParam);
+
+        return lambda.Compile();
+    }
+
+}
---
+++ MEPhysBone/PhysBoneGrabHelper.cs
@@ -0,0 +1,113 @@
+﻿/*
+ * com.vrchat.avatars-3.9.0\Runtime\VRCSDK\SDK3A\PhysBoneGrabHelper.cs
+*/
+using UnityEngine;
+using VRC.Dynamics;
+
+namespace VRC.SDK3.Dynamics.PhysBone
+{
+    [AddComponentMenu("")]
+    public class PhysBoneGrabHelper : MonoBehaviour
+    {
+        Camera currentCamera;
+
+        void Start()
+        {
+            CurrentModel.OnAwake(); // in Start() bc Awake() plays before it should when component gets added on RuntimeInitializeLoadType.BeforeSceneLoad
+
+            currentCamera = Camera.main;
+        }
+        void Update()
+        {
+            CurrentModel.OnUpdate();
+
+            if (CurrentModel.ModelGO == null) return;
+
+            if (!CurrentModel.AvatarBigScreenHandlerProxy.isBigScreenActive) return;
+            
+
+            //Process mouse input
+            SetMouseDown(Input.GetMouseButton(0));
+            if (mouseIsDown && Input.GetMouseButtonDown(1))
+            {
+                if(grab != null)
+                {
+                    PhysBoneManager.Inst.ReleaseGrab(grab, true);
+                    grab = null;
+                }
+            }
+            UpdateGrab();
+        }
+
+
+        bool mouseIsDown = false;
+        PhysBoneManager.Grab grab;
+        Vector3 grabOrigin;
+        void SetMouseDown(bool state)
+        {
+            if (state == mouseIsDown)
+                return;
+
+            mouseIsDown = state;
+
+            if (mouseIsDown)
+            {
+                var ray = GetMouseRay();
+                grab = PhysBoneManager.Inst.AttemptGrab(-1, ray, out grabOrigin);
+                #if VERBOSE_LOGGING
+                if (grab != null)
+                {
+                    Debug.Log($"Grabbing - Chain:{grab.chainId} Bone:{grab.bone}");
+                }
+                #endif
+            }
+            else
+            {
+                if (grab != null)
+                {
+                    PhysBoneManager.Inst.ReleaseGrab(grab);
+                    grab = null;
+                }
+            }
+        }
+        Ray GetMouseRay()
+        {
+            if(currentCamera != null)
+                return currentCamera.ScreenPointToRay(Input.mousePosition);
+            else
+                return default;
+        }
+        void UpdateGrab()
+        {
+            if(currentCamera == null)
+                return;
+
+            if (grab != null)
+            {
+                var ray = GetMouseRay();
+                Vector3 hit;
+                if (PlaneLineIntersection(grabOrigin, -currentCamera.transform.forward, ray.origin, ray.origin + ray.direction * 1000f, out hit))
+                {
+                    grab.globalPosition = hit + (Vector3)grab.localOffset;
+                }
+            }
+        }
+        public static bool PlaneLineIntersection(Vector3 planeOrigin, Vector3 planeNormal, Vector3 lineA, Vector3 lineB, out Vector3 hit)
+        {
+            float delta;
+
+            //Make sure the line is not parallel
+            delta = Vector3.Dot(planeNormal, (lineB - lineA) - planeOrigin);
+            if (delta == 0.0f)
+            {
+                hit = Vector3.zero;
+                return false;
+            }
+
+            //Find the delta
+            delta = Vector3.Dot(planeNormal, lineA - planeOrigin) / delta;
+            hit = lineA + ((lineB - lineA) * -delta);
+            return true;
+        }
+    }
+}
